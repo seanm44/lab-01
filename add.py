e12fd696df740969108fedbd7d2c3787b887a0df
seanm44 <C00310393@setu.ie>
2025-09-15 11:09:51 +0000

    update

---
+++ add.py
@@ -0,0 +1,4 @@
+num1 = int(input("What is your first number "))
+num2 = int(input("What is the second number "))
+sum = num1 + num2
+print(sum)
---
+++ student.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+studentno = input("What is your student number? ")
+
+print("Your name is " + name)
+print("You are " + age + " years old")
+print("Your student number is " + studentno)
